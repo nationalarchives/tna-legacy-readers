ac80e44fcd65d3228ec37b8091a59d4f425af6fe
Punal Chotrani <punalsc@gmail.com>
2018-03-23 14:44:39 +0000

    using the new css toolkit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: {
+                    'css/tna-base.css': 'sass/base-sass.scss',
+                    'css/tna-forms.css': 'sass/forms-sass.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/tna-base.css.min': ['css/tna-base.css'],
+                    'css/tna-forms.css.min': ['css/tna-forms.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
+};
