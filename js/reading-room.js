8f975aa2f92c31be68489f8943df354caf3a3341
SimonW <swilkes@nationalarchives.gov.uk>
2021-12-17 14:30:52 +0000

    Minor changes

--- js/reading-room.js
+++ js/reading-room.js
@@ -4,7 +4,7 @@ $(".census-toggle").click(function(){
 
     $("#census-banner").toggle();
     $(".census-toggle").html(function(i, text){
-        return text === "Show <span class=\"sr-only\">1921 census banner</span>" ? "Hide <span class=\"sr-only\">1921 census banner</span>" : "Show <span class=\"sr-only\">1921 census banner</span>";
+        return text === "Show <span class=\"sr-only\">1921 Census links</span>" ? "Hide <span class=\"sr-only\">1921 Census links</span>" : "Show <span class=\"sr-only\">1921 Census links</span>";
     })
 
 
